b3375a5c1a879d6e585aba72a0164f7a0ce49bf6
kcs19 <changsung19@gmail.com>
2025-03-24 10:35:35 +0900

    task1. project push

---
+++ step07/src/main/java/edu/fisa/ce/Step07Application.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step07Application.class, args);
+	}
+
+}
---
+++ step07/src/main/java/edu/fisa/ce/controller/CICDController.java
@@ -0,0 +1,15 @@
+package edu.fisa.ce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CICDController {
+	
+	@GetMapping("fisa")
+	public String reqRes() {
+		System.out.println("reqRes()");
+		return "요청 응답 성공";
+	}
+
+}
